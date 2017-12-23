278f68488b90363a1cf5299567b232b6a831a89e
Jesper Eskilson <jesper@eskilson.se>
2017-12-23 10:05:07 +0100

    Day23 work in progress

---
+++ 2017/java/src/day23/Day23.java
@@ -0,0 +1,87 @@
+package day23;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import utils.Utils;
+
+public class Day23 {
+	enum Op {
+		set, sub, mul, jnz
+	}
+
+	static class Interpreter {
+		private Map<String, Long> regs = new HashMap<>();
+		private Map<Op, Long> stats = new HashMap<>();
+
+		public long valueOf(String s) {
+			try {
+				return Integer.valueOf(s);
+			} catch (NumberFormatException e) {
+				return regs.getOrDefault(s, 0L);
+			}
+		}
+
+		public Map<String, Long> getRegs() {
+			return regs;
+		}
+
+		public Map<Op, Long> getStats() {
+			return stats;
+		}
+
+		public void interpret(String[] program) {
+			int pc = 0;
+
+			while (pc >= 0 && pc < program.length) {
+				String instr = program[pc];
+				String[] ops = instr.split(" ");
+				Op opcode = Op.valueOf(ops[0].trim());
+
+				stats.put(opcode, stats.getOrDefault(opcode, 0L) + 1);
+
+				switch (opcode) {
+				case set: {
+					regs.put(ops[1], valueOf(ops[2]));
+					pc++;
+					break;
+				}
+				case sub: {
+					regs.put(ops[1], valueOf(ops[1]) - valueOf(ops[2]));
+					pc++;
+					break;
+				}
+				case mul: {
+					regs.put(ops[1], valueOf(ops[1]) * valueOf(ops[2]));
+					pc++;
+					break;
+				}
+				case jnz: {
+					if (valueOf(ops[1]) != 0) {
+						pc += valueOf(ops[2]);
+					} else {
+						pc++;
+					}
+					break;
+				}
+				default:
+					throw new AssertionError();
+				}
+			}
+		}
+	}
+
+	@Test
+	public void test1() throws Exception {
+		String[] program = Utils
+				.readFileFromClassPathAsLines(getClass(), "day23/input.txt")
+				.toArray(n -> new String[n]);
+
+		Interpreter interpreter = new Interpreter();
+		interpreter.interpret(program);
+
+		System.out.println("Instruction statistics: " + interpreter.getStats());
+	}
+}
