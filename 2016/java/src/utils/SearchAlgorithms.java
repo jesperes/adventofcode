f3cafc99c4a33f1c2b8d7f6ee6a53481393a942e
Jesper Eskilson <jesper@eskilson.se>
2018-01-12 06:48:47 +0100

    Work on 2016.11

--- 2016/java/src/utils/SearchAlgorithms.java
+++ 2016/java/src/utils/SearchAlgorithms.java
@@ -9,79 +9,73 @@ import java.util.Set;
 
 public class SearchAlgorithms {
 
-    public interface BreadthFirstSearch<Node> {
-        List<Node> getChildren(Node node);
+	public interface BreadthFirstSearch<Node> {
+		List<Node> getChildren(Node node);
 
-        boolean isSolution(Node node);
+		boolean isSolution(Node node);
 
-        Node getParent(Node node);
-    }
+		Node getParent(Node node);
+	}
 
-    /**
-     * Breadth-first search. Assumes that the graph is a tree, i.e. any node is
-     * reachable through a unique path.
-     * 
-     * @param root
-     * @param data
-     * @return
-     */
-    public static <Node> Optional<List<Node>> breadthFirstSearch(Node root,
-            BreadthFirstSearch<Node> data) {
+	/**
+	 * Breadth-first search. Assumes that the graph is a tree, i.e. any node is
+	 * reachable through a unique path.
+	 * 
+	 * @param root
+	 * @param data
+	 * @return
+	 */
+	public static <Node> Optional<List<Node>> breadthFirstSearch(Node root,
+			BreadthFirstSearch<Node> data) {
 
-        // The frontier along which we are searching.
-        Queue<Node> openSet = new LinkedList<Node>();
+		// The frontier along which we are searching.
+		Queue<Node> openSet = new LinkedList<Node>();
 
-        // Tracks the states we have already visited.
-        Set<Node> closedSet = new HashSet<>();
+		// Tracks the states we have already visited.
+		Set<Node> closedSet = new HashSet<>();
 
-        openSet.add(root);
-        while (!openSet.isEmpty()) {
-            Node node = openSet.remove();
+		openSet.add(root);
+		while (!openSet.isEmpty()) {
+			Node node = openSet.remove();
 
-            System.out.println("Picking node from queue: " + node);
+			/*
+			 * Check if this is a solution, and if so return.
+			 */
+			System.out.format("Checking solution... (%d left to check)%n",
+					openSet.size());
+			boolean isSolution = data.isSolution(node);
+			if (isSolution) {
+				return Optional.of(constructSolutionList(node, data));
+			}
 
-            /*
-             * Check if this is a solution, and if so return.
-             */
-            boolean isSolution = data.isSolution(node);
-            if (isSolution) {
-                System.out.println("Found solution: " + node);
-                return Optional.of(constructSolutionList(node, data));
-            }
+			for (Node child : data.getChildren(node)) {
 
-            for (Node child : data.getChildren(node)) {
+				if (closedSet.contains(child)) {
+					// We have already visited this state.
+					continue;
+				}
 
-                System.out.println("Checking child: " + child);
+				// If we already have this state in our list of states
+				// to check, do not add it again.
+				if (!openSet.contains(child)) {
+					openSet.add(child);
+				}
+			}
 
-                if (closedSet.contains(child)) {
-                    // We have already visited this state.
-                    System.out.println("Already visited: " + child);
-                    continue;
-                }
+			closedSet.add(node);
+		}
 
-                // If we already have this state in our list of states
-                // to check, do not add it again.
-                if (!openSet.contains(child)) {
-                    System.out
-                            .println("Unchecked node, add to queue: " + child);
-                    openSet.add(child);
-                }
-            }
+		return Optional.empty();
+	}
 
-            closedSet.add(node);
-        }
-
-        return Optional.empty();
-    }
-
-    private static <Node> List<Node> constructSolutionList(Node node,
-            BreadthFirstSearch<Node> data) {
-        LinkedList<Node> list = new LinkedList<>();
-        Node current = node;
-        list.add(current);
-        while ((current = data.getParent(current)) != null) {
-            list.add(0, current);
-        }
-        return list;
-    }
+	private static <Node> List<Node> constructSolutionList(Node node,
+			BreadthFirstSearch<Node> data) {
+		LinkedList<Node> list = new LinkedList<>();
+		Node current = node;
+		list.add(current);
+		while ((current = data.getParent(current)) != null) {
+			list.add(0, current);
+		}
+		return list;
+	}
 }
