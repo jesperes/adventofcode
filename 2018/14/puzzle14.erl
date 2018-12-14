8336ddb6bdf4a103ea53f4231418b5df32e1a6bf
Jesper Eskilson <jesper.eskilson@klarna.com>
2018-12-14 09:34:39 +0100

    day 14 solutions

---
+++ 2018/14/puzzle14.erl
@@ -0,0 +1,14 @@
+%%%-------------------------------------------------------------------
+%%% @author Jesper Eskilson <>
+%%% @copyright (C) 2018, Jesper Eskilson
+%%% @doc
+%%%
+%%% @end
+%%% Created : 14 Dec 2018 by Jesper Eskilson <>
+%%%-------------------------------------------------------------------
+-module(puzzle14).
+
+-export([start1/0]).
+
+start1() ->
+    ok.
