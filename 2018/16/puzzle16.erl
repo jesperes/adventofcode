15c82c82c1e8a8796108ef185f6c7c2145b31b94
Jesper Eskilson <jesper.eskilson@klarna.com>
2018-12-16 13:38:45 +0100

    day 16 solution part 1 and 2

--- 2018/16/puzzle16.erl
+++ 2018/16/puzzle16.erl
@@ -14,11 +14,13 @@ start() ->
     {ok, Binary} = file:read_file("input.txt"),
     Lines = string:tokens(binary_to_list(Binary), "\n"),
     List = parse_lines(Lines),
-    P = pass2(List),
+    Samples = pass2(List),
     
-    MatchingInstructions =
+    MatchedSamples = 
 	lists:map(
-	  fun({[before|RegValsBefore], [OpNum,A,B,C] = Instr, ['after'|RegValsAfter]}) ->
+	  fun({[before|RegValsBefore], 
+               [_OpNum,A,B,C] = Instr, 
+               ['after'|RegValsAfter]}) ->
 		  {sample, RegValsBefore, Instr, RegValsAfter, matches,
 		   lists:filtermap(fun({OpCode, RegValsAfter0}) ->
 					   case RegValsAfter0 == RegValsAfter of
@@ -30,15 +32,123 @@ start() ->
 				   end, 
 				   [{OpCode, execute_opcode(OpCode, A, B, C, RegValsBefore)}
 				    || OpCode <- opcodes()])}
-	  end, P),
-    
-    length(
+	  end, Samples),
+
+    %% Number of instructions which matches at least 3 samples.
+    Num = length(
       lists:filter(fun({sample, _, _, _, _, OpCodes}) ->
 			   length(OpCodes) >= 3
-		   end, MatchingInstructions)).
-	     			
+		   end, MatchedSamples)),
+
+    [R1|_] = execute_test_program(List),
+
+    {{part1, Num},
+     {part2, R1}}.
+
+%% {Num, match_opcodes(MatchedSamples), }.
+
+execute_test_program(List) ->
+    InstrList = pass2_testprog(List),
+    execute_instr_list(InstrList, [0,0,0,0]).
+
+execute_instr_list([], RegVals) ->
+    RegVals;
+execute_instr_list([[OpNum, A, B, C]|Rest], RegVals) ->
+    OpCode = opcode(OpNum),
+    NewRegVals = execute_opcode(OpCode, A, B, C, RegVals),
+    execute_instr_list(Rest, NewRegVals).
+
+%% This is a throwaway function to obtain the opnum to opcode mapping,
+%% see opcode/1.
+match_opcodes(MatchedSamples) ->
+    %% MatchedSamples is a list of {sample, Before, Instr, After,
+    %% OpcodeList} when OpcodeList is a list of opcodes which match
+    %% the Before/Instr/After sample.
     
-   
+    L = 
+        [ lists:filtermap(fun({sample, _, [SampleOpNum|_], _, _, MatchingOpCodes}) ->
+                                  case SampleOpNum == OpNum of
+                                      true ->
+                                          {true, {OpNum, sets:from_list(MatchingOpCodes)}};
+                                      _ ->
+                                          false
+                                  end
+                          end, MatchedSamples)
+          || OpNum <- lists:seq(0, length(opcodes()) - 1) ],
+
+    %% L is now a list of tuples {opnum, matching_opcodes}, so we take
+    %% the intersection of all the matching opcodes.
+
+    lists:map(fun([{OpNum, _}|_] = OpCodeMatches) ->
+                      
+                      Sets = 
+                          lists:map(fun({_, MatchingOpCodes}) ->
+                                            MatchingOpCodes
+                                    end, OpCodeMatches),
+                      ISet = sets:intersection(Sets),
+                      
+                      {OpNum, sets:to_list(ISet)};
+                 (_) ->
+                      {}
+              end, L).
+
+
+%% {521,
+%%  [{0,[eqir,borr,addr,eqri,seti]},
+%%   {1,[borr,addi,bori,seti]},
+%%   {2,[eqrr,eqir,seti]},
+%%   {3,[eqrr,gtir,seti]},
+%%   {4,[addi]},
+%%   {5,[gtrr,gtir,setr,borr,addr,gtri,seti,bori,addi]},
+%%   {6,[gtrr,eqri]},
+%%   {7,[gtir,seti]},
+%%   {8,[muli]},
+%%   {9,[borr,bori]},
+%%   {10,
+%%    [gtrr,gtir,setr,borr,banr,mulr,addr,gtri,seti,bori,bani,
+%%     muli,addi]},
+%%   {11,[borr]},
+%%   {12,[eqrr,eqir,gtrr,gtir,eqri,gtri,seti]},
+%%   {13,[eqrr,gtir,setr,borr,banr,mulr,eqri,seti,bori,addi]},
+%%   {14,[mulr,addr,muli,addi]},
+%%   {15,[mulr,addi]}]}
+%%
+%% From this we can deduce that:
+%%
+%% 0 = eqri
+%% 1 = seti
+%% 2 = eqir
+%% 3 = eqrr
+%% 4 = addi
+%% 5 = setr
+%% 6 = gtrr
+%% 7 = gtir
+%% 8 = muli
+%% 9 = bori
+%% 10 = bani
+%% 11 = borr
+%% 12 = gtri
+%% 13 = banr
+%% 14 = addr
+%% 15 = mulr
+%%
+
+opcode(0) -> eqri;
+opcode(1) -> seti;
+opcode(2) -> eqir;
+opcode(3) -> eqrr;
+opcode(4) -> addi;
+opcode(5) -> setr;
+opcode(6) -> gtrr;
+opcode(7) -> gtir;
+opcode(8) -> muli;
+opcode(9) -> bori;
+opcode(10) -> bani;
+opcode(11) -> borr;
+opcode(12) -> gtri;
+opcode(13) -> banr;
+opcode(14) -> addr;
+opcode(15) -> mulr.
 
 parse_lines([]) ->
     [];
@@ -62,6 +172,13 @@ pass2([[before|_] = Before, Instr, ['after'|_] = After|Rest]) ->
     [{Before, Instr, After}|pass2(Rest)];
 pass2([_|Rest]) ->
     pass2(Rest).
+
+pass2_testprog([]) ->
+    [];
+pass2_testprog([[before|_], _, ['after'|_]|Rest]) ->
+    pass2_testprog(Rest);
+pass2_testprog([Instr|Rest]) ->
+    [Instr|pass2_testprog(Rest)].
 
 opcodes() -> 
     [addr, addi, mulr, muli, banr, bani, borr, bori, setr,
