c05c6c54762f99a047e8905bfc41d88dd5c1085a
Jesper Eskilson <jesper.eskilson@klarna.com>
2018-12-21 18:22:20 +0100

    day 16 and 17

--- 2015/16/puzzle16.erl
+++ 2015/16/puzzle16.erl
@@ -23,10 +23,24 @@ mfcsam_output() ->
       perfumes => 1
      }.
 
+
 matches(Map1, Map2) ->
     AllKeys = 
         sets:from_list(maps:keys(Map1) ++ 
                            maps:keys(Map2)).
+
+matches(cats, AuntVal, MfcSamValue) ->
+    AuntVal > MfcSamValue;
+matches(trees, AuntVal, MfcSamValue) ->
+    AuntVal > MfcSamValue;
+matches(pomeranians, AuntVal, MfcSamValue) ->
+    AuntVal < MfcSamValue;
+matches(goldfish, AuntVal, MfcSamValue) ->
+    AuntVal < MfcSamValue;
+matches(_, AuntVal, MfcSamValue) ->
+    AuntVal == MfcSamValue.
+
+
 
 start() ->
     {ok, Binary} = file:read_file("input.txt"),
@@ -37,11 +51,20 @@ start() ->
                             {list_to_integer(Num),
                              list_to_dict(Rest)}
                     end, string:tokens(binary_to_list(Binary), "\r\n"))),
-
     
-    
+    MfcSam = mfcsam_output(),
+    MfcSamKeys = sets:from_list(maps:keys(MfcSam)),
 
-
+    maps:filter(fun(AuntNum, M) ->
+                        Keys1 = sets:from_list(maps:keys(M)),
+                        CommonKeys = 
+                            sets:intersection(Keys1, MfcSamKeys),
+                        lists:all(fun(K) ->
+                                          V1 = maps:get(K, M), 
+                                          V2 = maps:get(K, MfcSam),
+                                          matches(K, V1, V2)
+                                  end, sets:to_list(CommonKeys))
+                end, Map).
 
 list_to_dict([]) ->
     #{};
