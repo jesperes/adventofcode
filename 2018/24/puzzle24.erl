6c51c2390d411170ad32898bf8487a474c496bc1
Jesper Eskilson <jesper.eskilson@klarna.com>
2018-12-24 20:46:50 +0100

    parser for day 24

--- 2018/24/puzzle24.erl
+++ 2018/24/puzzle24.erl
@@ -8,42 +8,65 @@
 -module(puzzle24).
 -compile([export_all]).
 
+-define(UNIT_RE, <<"(\\d+) units each with (\\d+) hit points (\\((.*)\\) )?with an attack that does (\\d+) (\\w+) damage at initiative (\\d+)">>).
+
 start() ->
     input("input.txt").
 
 input(Filename) ->
     {ok, Binary} = file:read_file(Filename),
-    Lines = string:tokens(binary_to_list(Binary), "\r\n"),
-    lists:map(fun(Line) ->
-		      parse_line(Line)
-	      end, Lines).
+    [_, _, ImmuneSystem, _, Infection] =
+        re:split(binary_to_list(Binary), "(Immune System|Infection):"),
+    {ImmuneSystem, Infection},
+    {{immune_system, parse_army(ImmuneSystem)}, 
+     {infection, parse_army(Infection)}}.
 
-capture_groups(Line, Captures) ->
-    lists:map(fun(Capture) ->
-		      case Capture of
-			  {-1, _} ->
-			      "";
-			  {Start, Len} ->
-			      lists:sublist(Line, Start + 1, Len)
-		      end
-	      end, Captures).
+parse_army(Binary) ->
+    Groups = string:tokens(binary_to_list(Binary), "\n"),
+    lists:foldl(fun(Line, List) ->
+                        [parse_group(Line)|List]
+                end, [], Groups).
 
-parse_line(Line) ->
-    case re:run(Line, "(\\d+) units each with (\\d+) hit points (\\(.*\\) )?with an attack that does (\\d+) (\\w+) damage at initiative (\\d+)") of
-	{match, Captures} ->
-	    [_, Units, HP, Weakness, Damage, DamageType, Initiative] =
-		capture_groups(Line, Captures),
-	    #{ units => list_to_integer(Units),
-	       hp => list_to_integer(HP),
-	       weakness => parse_weakness(Weakness),
-	       damage => list_to_integer(Damage),
-	       damagetype => DamageType,
-	       initiative => list_to_integer(Initiative)};
-	N ->
-	    {nomatch, Line}
+to_i(N) -> list_to_integer(N).
+to_a(N) -> list_to_atom(N).
+     
+parse_group(Line) ->
+    case re:run(Line, ?UNIT_RE) of
+        {match, Captures} ->
+            #{units => to_i(capture_group(Line, Captures, 1)),
+              hp => to_i(capture_group(Line, Captures, 2)),
+              damage => to_i(capture_group(Line, Captures, 5)),
+              damagetype => to_a(capture_group(Line, Captures, 6)),
+              initiative => to_i(capture_group(Line, Captures, 7)),
+              strengths => parse_strengths("immune", capture_group(Line, Captures, 4)),
+              weaknesses => parse_strengths("weak", capture_group(Line, Captures, 4))
+             }
+    end.
+            
+        
+parse_strengths(What, Str) ->
+    X = parse_strength(What, 
+                       lists:map(fun string:trim/1, string:tokens(Str, ";"))),
+    erlang:display({What, X}),
+    X.
+
+parse_strength(_, []) ->
+    [];
+parse_strength(What, [Strength|Rest]) ->
+    Tokens = string:tokens(Strength, " ,"),
+    erlang:display({tokens, Tokens}),
+    case Tokens of
+        [What, "to"|Thing] ->
+            lists:map(fun to_a/1, Thing);
+        _ ->
+            parse_strength(What, Rest)
     end.
 
-parse_weakness([]) ->
-    [];
-parse_weakness(Weakness) ->
-    
+capture_group(Line, Captures, N) ->
+    Capture = lists:nth(N + 1, Captures),
+    case Capture of
+        {-1, _} ->
+            "";
+        {Start, Len} ->
+            lists:sublist(Line, Start + 1, Len)
+    end.
