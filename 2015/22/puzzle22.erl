f6a3ce44c009ec0e48f6762fb55fd1246817e9fb
Jesper Eskilson <jesper@eskilson.se>
2019-01-01 17:43:42 +0100

    2015/22 wip

---
+++ 2015/22/puzzle22.erl
@@ -0,0 +1,13 @@
+%%% @author  <jespe@LAPTOP-P6HKA27J>
+%%% @copyright (C) 2018, 
+%%% @doc
+%%%
+%%% @end
+%%% Created : 31 Dec 2018 by  <jespe@LAPTOP-P6HKA27J>
+
+-module(puzzle22).
+-compile([export_all]).
+
+start() ->
+    ok.
+	 
