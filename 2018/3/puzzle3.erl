2b456167c0bbf6192bba426aed0edb8031b68815
Jesper Eskilson <jesper.eskilson@klarna.com>
2019-01-17 14:36:57 +0100

    Improved day 3 solution to run in under a second.

--- 2018/3/puzzle3.erl
+++ 2018/3/puzzle3.erl
@@ -1,96 +1,94 @@
 -module(puzzle3).
 -export([main/0]).
+-compile([export_all]).
 
 main() ->
-    {{part1, start1()},
-     {part2, start2()}}.
+    Areas = get_areas(),
+    {{part1, start1(Areas)},
+     {part2, start2(Areas)}}.
 
-start1() ->
-    %% TestData = ["#1 @ 1,3: 4x4",
-    %%             "#2 @ 3,1: 4x4",
-    %%             "#3 @ 5,5: 2x2"],
-    
-    Input = read_file_lines("input.txt"),
-    Areas = lists:map(fun parse_line/1, Input),
+start1(Areas) ->
     ClaimedAreas = count_claims(Areas, #{}),
+    count_overlaps(ClaimedAreas).
 
-    maps:fold(fun(_, Claims, AccIn) ->
-                      if length(Claims) > 1 ->
-                              1 + AccIn;
-                         true ->
-                              AccIn
-                      end
-              end, 0, ClaimedAreas).
+start2(Areas) ->
+    [{Id, _, _, _, _}|_]
+        = lists:dropwhile(fun(A) ->
+                                  overlaps(A, Areas)
+                          end, Areas),
+    Id.
 
-read_file_lines(File) ->
-    {ok, Binary} = file:read_file(File),
-    string:tokens(binary_to_list(Binary), "\n").
+%%% Part 1
 
-parse_line(Line) ->
-    [Id, Left, Top, W, H] = 
-        lists:map(fun list_to_integer/1, string:tokens(Line, "#@ ,:x")),
-    {Id, Left, Top, W, H}.
+%% Count the number of square inches which overlap by more than 2.
+count_overlaps(Map) ->
+    count_overlaps0(maps:iterator(Map), 0).
 
+count_overlaps0(It0, N) ->
+    case maps:next(It0) of
+        {_, V, It1} when V >= 2 ->
+            count_overlaps0(It1, N + 1);
+        {_, _, It1} ->
+            count_overlaps0(It1, N);
+        _ ->
+            N
+    end.
+
+
+%% Returns a map from Pos -> NumberOfClaims, one position for each
+%% square inch.
 count_claims([], Map) ->
     Map;
 count_claims([{Id, L, T, W, H}|Areas], Map) ->
     NewMap = claim_area(Id, L, T, W, H, Map),
     count_claims(Areas, NewMap).
 
-claim_area(Id, L, T, W, H, Map) ->
-
-    %% The coordinates for each square inch in the claimed area.
-    Coords = 
+claim_area(_, L, T, W, H, Map) ->
+    Coords =
         [{X,Y} || X <- lists:seq(L, L + W - 1),
                   Y <- lists:seq(T, T + H - 1)],
-    
-    %% Map is a map of {X,Y} coordinates to lists of ids making claims
-    %% on that particular square inch.
     lists:foldl(fun(K, AccIn) ->
-                        maps:update_with(K, 
+                        maps:update_with(K,
                                          fun(V) ->
-                                                 [Id|V]
-                                         end, [Id], AccIn)
+                                                 V + 1
+                                         end, 1, AccIn)
                 end, Map, Coords).
 
-%% display_fabric(W, H, Map) ->
-%%     io:format("~p~n", 
-%%               [[[maps:get({X,Y},Map,[])
-%%                  || X <- lists:seq(0, W - 1)]
-%%                 || Y <- lists:seq(0, H - 1)]]).
 
-start2() ->
-    %% TestData = ["#1 @ 1,3: 4x4",
-    %%             "#2 @ 3,1: 4x4",
-    %%             "#3 @ 5,5: 2x2"],
+%%% Part 2
 
+%% Does area A overlap any area in Areas?
+overlaps(A, Areas) ->
+    lists:any(fun(A1) ->
+                      overlaps0(A, A1)
+              end, Areas).
+
+overlaps0(A, A) ->
+    false;
+overlaps0(A1, A2) ->
+    {_, L1, T1, W1, H1} = A1,
+    {_, L2, T2, W2, H2} = A2,
+
+    %% Each of these are true if the two areas do not overlap (either
+    %% side-by-side or over-and-under, or both). If they are all
+    %% false, then the two areas overlap.
+    not ((L1 + W1 =< L2) or
+         (L2 + W2 =< L1) or
+         (T1 + H1 =< T2) or
+         (T2 + H2 =< T1)).
+
+
+%%% Parser
+
+get_areas() ->
     Input = read_file_lines("input.txt"),
-    Areas = lists:map(fun parse_line/1, Input),
-    ClaimedAreas = count_claims(Areas, #{}),
+    lists:map(fun parse_line/1, Input).
 
-    AllIds = lists:map(fun({Id, _, _, _, _}) ->
-                               Id
-                       end, Areas),
-    %% Find the id which does not share any square in with any other
-    %% id.
-    [N] = lists:filter(fun(Id) ->
-                               not overlaps(Id, ClaimedAreas)
-                       end, AllIds),
-    N.
+read_file_lines(File) ->
+    {ok, Binary} = file:read_file(File),
+    string:tokens(binary_to_list(Binary), "\n").
 
-overlaps(Id, ClaimedAreas) ->
-    %% List where each member is a list containing the ids for each
-    %% square inch.
-    SquareInches = maps:values(ClaimedAreas),
-    
-    %% Find the cells which are shared, i.e. has at least 2 ids in
-    %% them.
-    Shared = lists:filter(fun(SqInch) ->
-                                  length(SqInch) >= 2
-                          end, SquareInches),
-        
-    %% If the specified id is a member of any of the shared lists, it
-    %% overlaps with another id.
-    lists:any(fun(SqInch) ->
-                      lists:member(Id, SqInch)
-              end, Shared).
+parse_line(Line) ->
+    list_to_tuple(
+        lists:map(fun list_to_integer/1,
+                  string:tokens(Line, "#@ ,:x"))).
