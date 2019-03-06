31e4cbe9669f6776c12d99af247bd8eee8cc2400
Jesper Eskilson <jesper.eskilson@klarna.com>
2019-03-06 08:58:04 +0100

    merge part 1 and 2 into a single pass

--- 2015/06/puzzle06.erl
+++ 2015/06/puzzle06.erl
@@ -17,64 +17,44 @@ start() ->
                   end
           end, Lines),
     
-    {part1(Instrs), 
-     part2(Instrs)}.
+    process_instr(Instrs).
 
 toi(N) -> list_to_integer(N).
 
+process_instr(Instrs) ->     
+    process_instr(Instrs, grid_new()).
 
-part1(Instrs) ->     
-    part1(Instrs, grid_new()).
-
-part1([], Grid) ->
-    grid_size(Grid);
-part1([{toggle, From, To}|Rest], Grid) ->
-    part1(Rest, fold_xy(fun grid_toggle/2, Grid, From, To));
-part1([{turn_on, From, To}|Rest], Grid) ->
-    part1(Rest, fold_xy(fun grid_turn_on/2, Grid, From, To));
-part1([{turn_off, From, To}|Rest], Grid) ->
-    part1(Rest, fold_xy(fun grid_turn_off/2, Grid, From, To)).
-
-part2(Instrs) ->     
-    part2(Instrs, grid_new()).
-
-part2([], Grid) ->
-    grid_sum_keys(Grid);
-part2([{toggle, From, To}|Rest], Grid) ->
-    part2(Rest, fold_xy(fun grid_incr_2/2, Grid, From, To));
-part2([{turn_on, From, To}|Rest], Grid) ->
-    part2(Rest, fold_xy(fun grid_incr_1/2, Grid, From, To));
-part2([{turn_off, From, To}|Rest], Grid) ->
-    part2(Rest, fold_xy(fun grid_decr_1/2, Grid, From, To)).
+process_instr([], Grid) ->
+    grid_get_solution(Grid);
+process_instr([{toggle, From, To}|Rest], Grid) ->
+    process_instr(Rest, fold_xy(fun grid_toggle/2, Grid, From, To));
+process_instr([{turn_on, From, To}|Rest], Grid) ->
+    process_instr(Rest, fold_xy(fun grid_turn_on/2, Grid, From, To));
+process_instr([{turn_off, From, To}|Rest], Grid) ->
+    process_instr(Rest, fold_xy(fun grid_turn_off/2, Grid, From, To)).
 
 fold_xy(Fun, Init, {X0, Y0}, {X1, Y1}) ->
     lists:foldl(Fun, Init, 
                 [<<X:16, Y:16>> || X <- lists:seq(X0, X1),
                                    Y <- lists:seq(Y0, Y1)]).
+%% -- grid abstraction --
 
 grid_new() -> #{}.
 
-grid_size(Grid) -> maps:size(maps:filter(fun(_, V) -> V end, Grid)).
+grid_get_solution(Grid) -> 
+    maps:fold(fun(_, {Vs, Vb}, {S, B}) ->
+                      {if Vs -> S + 1; true -> S end, B + Vb}
+              end, {0, 0}, Grid).
 
 grid_toggle(Pos, Grid) ->
-    maps:update_with(Pos, fun(V) -> not V end, true, Grid).
+    maps:update_with(Pos, fun({S, B}) -> {not S, B + 2} end, {true, 2}, Grid).
 
 grid_turn_on(Pos, Grid) ->
-    maps:put(Pos, true, Grid).
+    maps:update_with(Pos, fun({_, B}) -> {true, B + 1} end, {true, 1}, Grid).
 
 grid_turn_off(Pos, Grid) ->
-    maps:remove(Pos, Grid).
+    maps:update_with(Pos, fun({_, B}) -> {false, max(0, B - 1)} end, {false, 0}, Grid).
 
-grid_incr_2(Pos, Grid) ->
-    maps:update_with(Pos, fun(V) -> V + 2 end, 2, Grid).
 
-grid_incr_1(Pos, Grid) ->
-    maps:update_with(Pos, fun(V) -> V + 1 end, 1, Grid).
-
-grid_decr_1(Pos, Grid) ->
-    maps:update_with(Pos, fun(V) -> max(V - 1, 0) end, 0, Grid).
-
-grid_sum_keys(Grid) ->                                   
-    lists:sum(maps:values(Grid)).
      
     
