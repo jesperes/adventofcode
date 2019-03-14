441e1481bc05c65e453017206e50bf114be0892c
Jesper Eskilson <jesper@eskilson.se>
2019-03-14 18:56:04 +0100

    2015 day 3 java

--- aoc-java/src/aoc2015/Day03.java
+++ aoc-java/src/aoc2015/Day03.java
@@ -1,5 +1,7 @@
 package aoc2015;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -8,17 +10,21 @@ import java.util.Set;
 
 import org.junit.Test;
 
+import common.IntPair;
+
 public class Day03 {
 
-    class Coord {
-        public int x, y;
+    class Santa {
+        private int x, y;
+        private Set<IntPair> houses;
 
-        public Coord(int x, int y) {
+        public Santa(int x, int y, Set<IntPair> houses) {
             this.x = x;
             this.y = y;
+            this.houses = houses;
         }
 
-        void move(char c) {
+        void move(int c) {
             switch (c) {
             case '<':
                 x--;
@@ -34,21 +40,40 @@ public class Day03 {
                 break;
             }
         }
+
+        void deliver() {
+            houses.add(new IntPair(x, y));
+        }
     }
 
     @Test
     public void testDay03() throws IOException {
-        Set<Integer> p1 = new HashSet<>();
-        Set<Integer> p2 = new HashSet<>();
+        Set<IntPair> p1 = new HashSet<>();
+        Set<IntPair> p2 = new HashSet<>();
 
-        Coord santa1 = new Coord(0, 0);
-        Coord santa2 = new Coord(0, 0);
-        Coord roboSanta = new Coord(0, 0);
+        Santa santa1 = new Santa(0, 0, p1);
+        Santa santa2 = new Santa(0, 0, p2);
+        Santa roboSanta = new Santa(0, 0, p2);
 
-        try (Reader reader = new FileReader("input/2015/day03.txt")) {
+        try (Reader reader = new FileReader("inputs/2015/day03.txt")) {
             int c;
+            int i = 0;
             while ((c = reader.read()) != -1) {
+                santa1.deliver();
+                santa2.deliver();
+                roboSanta.deliver();
+
+                santa1.move(c);
+                if (i % 2 == 0)
+                    santa2.move(c);
+                else
+                    roboSanta.move(c);
+
+                i++;
             }
         }
+
+        assertEquals(2572, p1.size());
+        assertEquals(2631, p2.size());
     }
 }
---
+++ aoc-java/src/common/IntPair.java
@@ -0,0 +1,7 @@
+package common;
+
+public class IntPair extends Pair<Integer, Integer> {
+    public IntPair(Integer first, Integer second) {
+        super(first, second);
+    }
+}
