63fcffbd8e199b90ee12d7c75191da06f26eaecf
Jesper Eskilson <jesper.eskilson@klarna.com>
2019-03-17 13:44:57 +0100

    code cleanup

--- aoc-java/src/common/IntTriple.java
+++
@@ -1,9 +0,0 @@
-package common;
-
-public class IntTriple extends Triple<Integer, Integer, Integer> {
-
-    public IntTriple(Integer x, Integer y, Integer z) {
-        super(x, y, z);
-    }
-
-}
--- aoc-java/src/common/Pair.java
+++ aoc-java/src/common/Pair.java
@@ -11,34 +11,13 @@ public class Pair<T0, T1> {
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((x == null) ? 0 : x.hashCode());
-        result = prime * result + ((y == null) ? 0 : y.hashCode());
-        return result;
+        return x.hashCode() ^ y.hashCode();
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        @SuppressWarnings("rawtypes")
-        Pair other = (Pair) obj;
-        if (x == null) {
-            if (other.x != null)
-                return false;
-        } else if (!x.equals(other.x))
-            return false;
-        if (y == null) {
-            if (other.y != null)
-                return false;
-        } else if (!y.equals(other.y))
-            return false;
-        return true;
+        @SuppressWarnings("unchecked")
+        Pair<T0, T1> other = (Pair<T0, T1>) obj;
+        return other.x.equals(x) && other.y.equals(y);
     }
-
 }
