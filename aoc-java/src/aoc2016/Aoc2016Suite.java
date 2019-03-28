d1f1bd351e68568ff184911e29927b31a1aff0c8
Jesper Eskilson <jesper@eskilson.se>
2019-03-28 07:11:05 +0100

    wip

---
+++ aoc-java/src/aoc2016/Aoc2016Suite.java
@@ -0,0 +1,11 @@
+package aoc2016;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+// @formatter:off
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+    })
+public class Aoc2016Suite {
+}
---
+++ aoc-java/src/aoc2016/Day01.java
@@ -0,0 +1,22 @@
+package aoc2016;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import org.junit.Test;
+
+public class Day01 {
+    @Test
+    public void testDay01() throws Exception {
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader("inputs/2016/day01.txt"))) {
+            String line;
+	    while ((line = reader.readLine()) != null) {
+	    	  String s[] = line.split(" ");
+		  // TODO continue
+	    }
+	}
+    }       
+}
