cfe9598a3206d531dabf4e0b85f026d2cbb4d7f8
Jesper Eskilson <jesper.eskilson@klarna.com>
2019-11-02 16:26:17 +0100

    More work on rebar app

--- aoc_erlang/apps/aoc_erlang/src/aoc2015_day01.erl
+++ aoc_erlang/apps/aoc_erlang/src/aoc2015_day01.erl
@@ -6,18 +6,18 @@
 %%% Created :  1 Mar 2019 by Jesper Eskilson <jesper.eskilson@klarna.com>
 
 -module(aoc2015_day01).
--behaviour(aoc_solution).
+-behavior(aoc_solution).
 
 -export([start/0]).
 
 start() ->
-    {ok, Bin} = file:read_file("input.txt"),
-    count_floors(Bin, 0, 0, undef).
+  Bin = aoc_inputs:get_as_binary(2015, 1),
+  count_floors(Bin, 0, 0, undef).
 
 count_floors(<<>>, Acc, _, Neg) -> {Acc, Neg};
 count_floors(Bin, Acc, Pos, undef) when Acc < 0 ->
-    count_floors(Bin, Acc, Pos, Pos);
+  count_floors(Bin, Acc, Pos, Pos);
 count_floors(<<$(,Rest/binary>>, Acc, Pos, Neg) ->
-    count_floors(Rest, Acc + 1, Pos + 1, Neg);
+  count_floors(Rest, Acc + 1, Pos + 1, Neg);
 count_floors(<<$),Rest/binary>>, Acc, Pos, Neg) ->
-    count_floors(Rest, Acc - 1, Pos + 1, Neg).
+  count_floors(Rest, Acc - 1, Pos + 1, Neg).
