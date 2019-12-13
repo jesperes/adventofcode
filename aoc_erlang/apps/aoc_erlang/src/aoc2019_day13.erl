47c3f8e590f237b06bba8f7ab69420c39ce339a9
Jesper Eskilson <jesper.eskilson@klarna.com>
2019-12-13 19:29:26 +0100

    [2019/13/erlang] Solved part 2.

--- aoc_erlang/apps/aoc_erlang/src/aoc2019_day13.erl
+++ aoc_erlang/apps/aoc_erlang/src/aoc2019_day13.erl
@@ -4,12 +4,32 @@
 -module(aoc2019_day13).
 -include_lib("eunit/include/eunit.hrl").
 
+-define(EMPTY, 0).
+-define(WALL, 1).
+-define(BLOCK, 2).
+-define(PADDLE, 3).
+-define(BALL, 4).
+
+%% Define ENABLE_TTY_OUTPUT to be able to call play() to see the game
+%% being played on the tty.
+
+%% -define(ENABLE_TTY_OUTPUT, 1).
+
+-ifdef(ENABLE_TTY_OUTPUT).
+
+-export([play/0]).
+
+%% Solve the puzzle and print the grid on stdout at the game is being
+%% played.
+play() ->
+  part2(get_input()).
+
+-endif.
+
 %% Puzzle solution
 part1(Prog) ->
   {_, Output} = intcode:execute(Prog),
-  Output0 = lists:reverse(Output),
-  io:format("~p~n", [Output0]),
-  count_blocks(Output0, #{}).
+  count_blocks(lists:reverse(Output), #{}).
 
 count_blocks([], Map) -> maps:size(Map);
 count_blocks([X, Y, 2|Rest], Map) ->
@@ -17,9 +37,83 @@ count_blocks([X, Y, 2|Rest], Map) ->
 count_blocks([_, _, _|Rest], Map) ->
   count_blocks(Rest, Map).
 
-%% part2(_Input) ->
-%%   ?debugMsg("Not implemented."),
-%%   0.
+%% ---- [ Part 2 ] ----
+
+-record(state, { score = 0
+               , output = {}
+               , grid = #{}
+               , paddle_x = 0 %% x coord of paddle
+               , ball_x = 0   %% x coord of ball
+               }).
+
+part2(Prog) ->
+  Prog0 = maps:merge(Prog, #{0 => 2}),
+  InitState = #state{},
+
+  {_, #state{score = Score}} =
+    intcode:execute(Prog0, fun joystick/1, fun display/2, InitState),
+
+  Score.
+
+
+joystick(#state{ball_x = Bx, paddle_x = Px} = State) ->
+  {State, case Bx - Px of
+            0 -> 0;
+            D when D < 0 -> -1;
+            _ -> 1
+          end}.
+
+%% Receive X coordinate
+display(X, #state{output = {}} = State) ->
+  State#state{output = {X}};
+
+%% Receive Y coordinate
+display(Y, #state{output = {X}} = State) ->
+  State#state{output = {X, Y}};
+
+%% Receive score
+display(Score, #state{output = {-1, 0}} = State) ->
+  State#state{output = {}, score = Score};
+
+%% Receive tile
+display(Tile, #state{output = {X, Y}, grid = Grid} = State) ->
+  State0 = State#state{grid = maps:put({X, Y}, Tile, Grid),
+                       output = {}},
+  print_grid(State0),
+  case Tile of
+    ?BALL -> State0#state{ball_x = X};
+    ?PADDLE -> State0#state{paddle_x = X};
+    _ -> State0
+  end.
+
+-ifdef(ENABLE_TTY_OUTPUT).
+
+print_grid(State) ->
+  io:format("~n~s~n~n", [grid_to_str(State)]).
+
+grid_to_str(#state{grid = Map, score = Score}) ->
+  {MinX, MaxX} = {-1, lists:max(lists:map(fun({X, _}) -> X end, maps:keys(Map)))},
+  {MinY, MaxY} = {0, lists:max(lists:map(fun({_, Y}) -> Y end, maps:keys(Map)))},
+  io_lib:format("Score: ~w~n~n", [Score]) ++
+    [ [to_str(X, Y, Map) ||
+        X <- lists:seq(MinX, MaxX) ] ++ "\n" ||
+      Y <- lists:seq(MinY, MaxY) ].
+
+to_str(X, Y, Map) ->
+  to_str(maps:get({X, Y}, Map, 0)).
+
+to_str(?EMPTY)  -> 32;
+to_str(?WALL)   -> $#;
+to_str(?BLOCK)  -> $*;
+to_str(?PADDLE) -> $=;
+to_str(?BALL)   -> $O.
+
+-else.
+
+print_grid(_) -> ok.
+
+-endif.
+
 
 %% Input reader (place downloaded input file in
 %% priv/inputs/2019/input13.txt).
@@ -31,7 +125,7 @@ main_test_() ->
   Input = get_input(),
 
   [ {"Part 1", ?_assertEqual(236, part1(Input))}
-  %% , {"Part 2", ?_assertEqual(0, part2(Input))}
+  , {"Part 2", ?_assertEqual(11040, part2(Input))}
   ].
 
 %%%_* Emacs ====================================================================
