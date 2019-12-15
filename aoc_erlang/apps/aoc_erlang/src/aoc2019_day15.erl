d0b83ad105b8f5203dd2285a751aef6eb2d83e3a
Jesper Eskilson <jesper.eskilson@klarna.com>
2019-12-15 14:26:03 +0100

    [2019/15/erlang] Solved part 2

--- aoc_erlang/apps/aoc_erlang/src/aoc2019_day15.erl
+++ aoc_erlang/apps/aoc_erlang/src/aoc2019_day15.erl
@@ -7,11 +7,20 @@
 %% Puzzle solution
 part1(Prog) ->
   Pid = start_intcode(Prog),
-  try
-    explore(Pid, #{pos => {0, 0}, dist => 0})
-  catch throw:{target, _, Steps} ->
-      Steps
-  end.
+  {Dist0, Grid0} =
+    try
+      explore(Pid, #{pos => {0, 0}, dist => 0, oxygen => not_found})
+    catch throw:{oxygen, Dist, Grid} ->
+        {Dist, Grid}
+    end,
+
+  Grid1 = fill_oxygen(Pid, Grid0),
+  OxygenDists =
+    lists:filtermap(fun({{_, _}, V}) -> {true, V};
+                       (_) -> false
+                    end, maps:to_list(Grid1)),
+
+  {Dist0, lists:max(OxygenDists)}.
 
 to_str(C) -> C.
 
@@ -56,11 +65,49 @@ explore(Pid, Grid0) ->
                 ?assertEqual(1, step_repair_robot(opposite(Dir), Pid)),
                 maps:merge(NewGrid, #{pos => Pos, dist => Dist});
               2 ->
-                %% Found target
-                throw({target, NewPos, NewDist})
+                %% Found oxygen source. Throw to wind back up (we want
+                %% to leave the intcode computer as is, and continue
+                %% with the oxygen-filling).
+                throw({ oxygen
+                      , NewDist,
+                        #{ pos => NewPos %% Where we are
+                         , NewPos => 0   %% Time to fill oxygen
+                         , time => 0
+                         }
+                      })
             end
         end
     end, Grid0, [1, 2, 3, 4]).
+
+fill_oxygen(Pid, Grid0) ->
+  lists:foldl(
+    fun(Dir, #{pos := Pos, time := Time} = Grid) ->
+        NewPos = move(Dir, Pos),
+        NewTime = Time + 1,
+        case maps:is_key(NewPos, Grid) of
+          true ->
+            T = maps:get(NewPos, Grid),
+            if Time < T -> maps:put(NewPos, Time, Grid);
+               true -> Grid
+            end;
+
+          false ->
+            case step_repair_robot(Dir, Pid) of
+              0 -> maps:put(NewPos, $#, Grid); %% Wall
+              1 ->
+                %% New pos is space. Robot was moved.
+                NewGrid =
+                  fill_oxygen(Pid, maps:merge(Grid, #{NewPos => NewTime,
+                                                      time => NewTime,
+                                                      pos => NewPos})),
+
+                step_repair_robot(opposite(Dir), Pid),
+                maps:merge(NewGrid, #{pos => Pos, time => Time})
+            end
+        end
+    end, Grid0, [1, 2, 3, 4]).
+
+
 
 move(1, {X, Y}) -> {X, Y - 1}; %% north
 move(2, {X, Y}) -> {X, Y + 1}; %% south
@@ -72,20 +119,13 @@ opposite(2) -> 1;
 opposite(3) -> 4;
 opposite(4) -> 3.
 
-part2(_Input) ->
-  %% ?debugMsg("Not implemented."),
-  0.
-
 get_input() ->
   intcode:parse(inputs:get_as_binary(2019, 15)).
 
 %% Tests
 main_test_() ->
   Input = get_input(),
-
-  [ {"Part 1", timeout, 60, ?_assertEqual(214, part1(Input))}
-  , {"Part 2", ?_assertEqual(0, part2(Input))}
-  ].
+  {"Part 1 & 2", ?_assertEqual({214, 344}, part1(Input))}.
 
 %%%_* Emacs ====================================================================
 %%% Local Variables:
