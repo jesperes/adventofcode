bc480afd2f324a6b3f0340eb8ccebfcba44ce9a4
Jesper Eskilson <jesper.eskilson@klarna.com>
2020-01-05 09:58:12 +0100

    Intcode implementation in C

--- aoc_c/src/intcode.c
+++ aoc_c/src/intcode.c
@@ -1,21 +1,13 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <string.h>
+#include <inttypes.h>
+#include <stdio.h>
 
 #include "intcode.h"
-
-/* Opcode definitions */
-#define OP_ADD 1
-#define OP_MUL 2
-#define OP_INPUT 3
-#define OP_OUTPUT 4
-#define OP_JUMP_IF_TRUE 5
-#define OP_JUMP_IF_FALSE 6
-#define OP_LESS_THAN 7
-#define OP_EQUALS 8
-#define OP_ADJ_RELBASE 9
-#define OP_END 99
 
 /* Addressing modes */
 #define MODE_POS 0
@@ -50,28 +42,76 @@ void write(int64_t *prog, int64_t op, int m, int64_t val, int relbase)
 
 void intcode_init(intcode_t *p)
 {
-};
+  memset(p, 0, sizeof(intcode_t));
+}
 
-/*
- * Execute the given intcode program.
- *
- * prog    The program, as an array of 64-bit ints. This
- *         should be malloc:ed, as it will be realloc:ed as needed.
- * progc   Size of the allocated buffer.
- * input   Array of inputs.
- * inputc  Size of input array.
- *
- * Returns a malloc:ed array of outputs.
- */
-void intcode_execute(intcode_t *p)
+// Read an intcode program from file.
+void intcode_init_from_file(intcode_t *p, char *filename)
 {
-  int pc = 0;
-  size_t relbase = 0;
-  int64_t *prog = p->prog;
+  intcode_init(p);
+
+  FILE *f = fopen(filename, "r");
+  assert(f != NULL);
+
+  char buf[20];
+  memset(buf, 0, sizeof(buf));
+  char *q = buf;
+
+  int progsize = 64; // Size of the intcode program (nr. of ints)
+  int i = 0; // index into the intcode program's array of ints
+  p->prog = (int64_t *)malloc(progsize * sizeof(int64_t));
 
   while (true) {
+    int c = getc(f);
+
+    // Reallocate program buffer when needed
+    if (i >= progsize) {
+      progsize *= 2;
+      p->prog = (int64_t *)realloc(p->prog, progsize * sizeof(int64_t));
+    }
+
+    if (c == ',' || c == EOF) {
+      p->prog[i++] = atoll(buf);
+      buf[0] = 0;
+      q = buf;
+      memset(buf, 0, sizeof(buf));
+      if (c == EOF)
+        break;
+    } else {
+      *q++ = c;
+    }
+  }
+
+  progsize *= 2;
+  p->prog = (int64_t *)realloc(p->prog, progsize * sizeof(int64_t));
+
+  // Zero out rest of program.
+  for (int j = i; j < progsize; j++) {
+    p->prog[j] = 0;
+  }
+
+  p->progsize = progsize;
+}
+
+void intcode_execute(intcode_t *p)
+{
+  while (true) {
+    int64_t pc = p->pc;
+    int relbase = p->relbase;
+    int64_t *prog = p->prog;
+
     // Read opcode
     int64_t op0 = prog[pc] % 100;
+
+#if 0
+    printf("pc = %" PRId64
+           ", opcode = %" PRId64
+           ", prog[pc] = %" PRId64 "\n", pc, op0, prog[pc]);
+#endif
+
+    // Store last opcode for OP_END, OP_INPUT, and OP_OUTPUT
+    // instructions.
+    p->last_opcode = op0;
 
     // Read opcode modifiers
     int64_t m1 = (prog[pc] / 100) % 10;
@@ -88,29 +128,29 @@ void intcode_execute(intcode_t *p)
       int64_t a = read(prog, op1, m1, relbase);
       int64_t b = read(prog, op2, m2, relbase);
       write(prog, op3, m3, a + b, relbase);
-      pc += 4;
+      p->pc += 4;
       break;
     }
     case OP_MUL: {
       int64_t a = read(prog, op1, m1, relbase);
       int64_t b = read(prog, op2, m2, relbase);
       write(prog, op3, m3, a * b, relbase);
-      pc += 4;
+      p->pc += 4;
       break;
     }
     case OP_JUMP_IF_TRUE: {
       if (read(prog, op1, m1, relbase)) {
-        pc = read(prog, op2, m2, relbase);
+        p->pc = read(prog, op2, m2, relbase);
       } else {
-        pc += 3;
+        p->pc += 3;
       }
       break;
     }
     case OP_JUMP_IF_FALSE: {
       if (read(prog, op1, m1, relbase)) {
-        pc += 3;
+        p->pc += 3;
       } else {
-        pc = read(prog, op2, m2, relbase);
+        p->pc = read(prog, op2, m2, relbase);
       }
       break;
     }
@@ -118,30 +158,135 @@ void intcode_execute(intcode_t *p)
       int64_t a = read(prog, op1, m1, relbase);
       int64_t b = read(prog, op2, m2, relbase);
       write(prog, op3, m3, (a < b) ? 1 : 0, relbase);
-      pc += 4;
+      p->pc += 4;
       break;
     }
     case OP_EQUALS: {
       int64_t a = read(prog, op1, m1, relbase);
       int64_t b = read(prog, op2, m2, relbase);
       write(prog, op3, m3, (a == b) ? 1 : 0, relbase);
-      pc += 4;
+      p->pc += 4;
       break;
     }
     case OP_ADJ_RELBASE: {
-      relbase += read(prog, op1, m1, relbase);
-      pc += 2;
+      p->relbase += read(prog, op1, m1, relbase);
+      p->pc += 2;
       break;
     }
     case OP_INPUT: {
+      if (!p->input_pending) {
+        // Return to user to handle input instruction.
+        p->input_pending = true;
+        return;
+      } else {
+        // We have received input from user, write it to
+        // memory and continue.
+        write(prog, op1, m1, p->input, relbase);
+        p->input_pending = false;
+        p->pc += 2;
+        break;
+      }
+    }
+    case OP_OUTPUT: {
+      if (!p->output_pending){
+        // Return to user to handle output instruction.
+        p->output = read(prog, op1, m1, relbase);
+        p->output_pending = true;
+        return;
+      } else {
+        // User has handled output instruction, continue.
+        p->output_pending = false;
+        p->pc += 2;
+        break;
+      }
+    }
+    case OP_END: {
+      return;
+    }
+    }
+  }
+}
+
+#ifdef INTCODE_ASCII_MAIN
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+/*
+ * Intcode computer hooked up to an ascii-capable terminal.
+ */
+int main(int argc, char **argv)
+{
+  assert(argc == 2);
+
+  // The intcode program state
+  intcode_t intcode;
+  intcode_init_from_file(&intcode, argv[1]);
+
+  while (true) {
+    intcode_execute(&intcode);
+    // printf("Intcode exited with opcode %d\n", intcode.last_opcode);
+    switch (intcode.last_opcode) {
+    case OP_END: {
+      return 0;
+    }
+    case OP_INPUT: {
+      intcode.input = getc(stdin);
       break;
     }
     case OP_OUTPUT: {
-      break;
-    }
-    case OP_END: {
+      // putchar(intcode.output);
       break;
     }
     }
   }
 }
+#endif
+
+#ifdef INTCODE_MAIN
+int main(int argc, char **argv)
+{
+  assert(argc >= 2);
+  int i = 2;
+
+  // The intcode program state
+  intcode_t intcode;
+  intcode_init_from_file(&intcode, argv[1]);
+
+  // intcode.prog[1] = 12;
+  // intcode.prog[2] = 2;
+
+  for (int j = 0; j < intcode.progsize; j++) {
+    printf("%" PRId64 ",", intcode.prog[j]);
+  }
+  printf("\n");
+
+  while (true) {
+    intcode_execute(&intcode);
+
+#if 0
+    printf("Execute function returned with opcode %d\n", intcode.last_opcode);
+    printf("Input pending: %d\n", intcode.input_pending);
+    printf("Output pending: %d\n", intcode.output_pending);
+#endif
+    switch (intcode.last_opcode) {
+    case OP_END: {
+      printf("Program exiting, prog[0] = %" PRId64 "\n",
+             intcode.prog[0]);
+      return 0;
+    }
+    case OP_INPUT: {
+      assert(i < argc);
+      intcode.input = atol(argv[i++]);
+      printf("INPUT = %" PRId64 "\n", intcode.input);
+      break;
+    }
+    case OP_OUTPUT: {
+      printf("%" PRId64 "\n", intcode.output);
+      break;
+    }
+    }
+  }
+}
+#endif
--- aoc_c/src/intcode.h
+++ aoc_c/src/intcode.h
@@ -2,34 +2,60 @@
 #define _INTCODE_H
 
 #include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+/* Opcode definitions */
+#define OP_ADD 1
+#define OP_MUL 2
+#define OP_INPUT 3
+#define OP_OUTPUT 4
+#define OP_JUMP_IF_TRUE 5
+#define OP_JUMP_IF_FALSE 6
+#define OP_LESS_THAN 7
+#define OP_EQUALS 8
+#define OP_ADJ_RELBASE 9
+#define OP_END 99
 
 /*
  * Intcode state
  */
 typedef struct {
-  int pc;
-  int relbase;
-  int64_t *prog;
-  size_t progc;
-  int64_t *input;
-  size_t inputc;
-  int64_t *output;
-  size_t outputc;
+  int pc;                       /* Program counter */
+  int relbase;                  /* For relative parameter modes */
+
+  int64_t *prog;                /* Intcode program */
+  int progsize;
+
+  int last_opcode;
+
+  bool input_pending;
+  int64_t input;
+
+  bool output_pending;
+  int64_t output;
+
 } intcode_t;
 
-typedef enum
-  {
-   StatusInput,
-   StatusOutput,
-  } IntcodeStatusCode;
-
-typedef struct {
-  IntcodeStatusCode status;
-  int64_t output;
-} intcode_status_t;
-
 void intcode_init(intcode_t *p);
+
+/*
+ * Execute intcode program. The function returns when it encounters a
+ * OP_END, OP_INPUT, or OP_OUTPUT instruction. The instruction can be
+ * found in the "last_opcode" field of the intcode_t struct.
+ *
+ * OP_END: The program has terminated.
+ *
+ * OP_INPUT: The program is waiting for input. Write the input value
+ *           to the "input" field.
+ *
+ * OP_OUTPUT: The program has produced output. Output value is stored
+ *            in the "output" field.
+ *
+ * To continue executing, call intcode_execute() again.
+ */
 void intcode_execute(intcode_t *p);
-void intcode_deinit(intcode_t *p);
+
+void intcode_init_from_file(intcode_t *p, char *filename);
 
 #endif
