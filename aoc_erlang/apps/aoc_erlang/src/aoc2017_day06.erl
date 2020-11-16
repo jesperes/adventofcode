ff4ba2ca42d9d34256f5f74f5a54ea4a6e271ba2
Jesper Eskilson <jesper.eskilson@klarna.com>
2020-11-16 19:42:08 +0100

    Comment out unused code

--- aoc_erlang/apps/aoc_erlang/src/aoc2017_day06.erl
+++ aoc_erlang/apps/aoc_erlang/src/aoc2017_day06.erl
@@ -5,32 +5,32 @@
 -include_lib("eunit/include/eunit.hrl").
 
 %% Puzzle solution
-part1(Input) ->
-  Banks = Input,
-  Len = length(Banks),
-  {Size, Num} = find_fullest_bank(Banks),
-  Banks0 = lists:keyreplace(Num, 2, Banks, {0, Num}),
-  redistribute(Banks0, (Num + 1) rem Len, Size, Len).
+%% part1(Input) ->
+%%   Banks = Input,
+%%   Len = length(Banks),
+%%   {Size, Num} = find_fullest_bank(Banks),
+%%   Banks0 = lists:keyreplace(Num, 2, Banks, {0, Num}),
+%%   redistribute(Banks0, (Num + 1) rem Len, Size, Len).
 
-part2(_Input) ->
-  ?debugMsg("Not implemented."),
-  0.
+%% part2(_Input) ->
+%%   ?debugMsg("Not implemented."),
+%%   0.
 
-redistribute(Banks, _, 0, _) ->
-  Banks;
-redistribute(Banks, Bank, Memory, Len) ->
-  {value, {Size, _}, Banks0} = lists:keytake(Bank, 2, Banks),
-  Banks1 = lists:keystore(Bank, 2, Banks0, {Size + 1, Bank}),
-  redistribute(Banks1, (Bank + 1) rem Len, Memory - 1, Len).
+%% redistribute(Banks, _, 0, _) ->
+%%   Banks;
+%% redistribute(Banks, Bank, Memory, Len) ->
+%%   {value, {Size, _}, Banks0} = lists:keytake(Bank, 2, Banks),
+%%   Banks1 = lists:keystore(Bank, 2, Banks0, {Size + 1, Bank}),
+%%   redistribute(Banks1, (Bank + 1) rem Len, Memory - 1, Len).
 
-find_fullest_bank(Banks) ->
-  [{S, N}|_] = lists:reverse(lists:sort(Banks)),
-  {S, N}.
+%% find_fullest_bank(Banks) ->
+%%   [{S, N}|_] = lists:reverse(lists:sort(Banks)),
+%%   {S, N}.
 
-get_input() ->
-  Banks = [2, 8, 8, 5, 4, 2, 3, 1, 5, 5, 1, 2, 15, 13, 5, 14],
-  Indexes = lists:seq(1, length(Banks)),
-  lists:zip(Banks, Indexes).
+%% get_input() ->
+%%   Banks = [2, 8, 8, 5, 4, 2, 3, 1, 5, 5, 1, 2, 15, 13, 5, 14],
+%%   Indexes = lists:seq(1, length(Banks)),
+%%   lists:zip(Banks, Indexes).
 
 %% Tests
 main_test_() ->
@@ -42,14 +42,14 @@ main_test_() ->
   %% , {"Part 2", ?_assertEqual(0, part2(Input))}
   %% ].
 
-redistribute_test() ->
-  Banks = [ {0, 1}
-          , {2, 2}
-          , {7, 3}
-          , {0, 4}
-          ],
+%% redistribute_test() ->
+%%   Banks = [ {0, 1}
+%%           , {2, 2}
+%%           , {7, 3}
+%%           , {0, 4}
+%%           ],
 
-  ?assertEqual(ok, redistribute(Banks, 3, 7, 4)).
+%%   ?assertEqual(ok, redistribute(Banks, 3, 7, 4)).
 
 
 %%%_* Emacs ====================================================================
