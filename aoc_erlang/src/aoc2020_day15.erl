83fbdce3aab382850d01ff84e00703f0ef4ef19a
Jesper Eskilson <jesper.eskilson@klarna.com>
2020-12-15 20:39:04 +0100

    2020 day 15 solution improvements

--- aoc_erlang/src/aoc2020_day15.erl
+++ aoc_erlang/src/aoc2020_day15.erl
@@ -12,13 +12,8 @@ speak([F|_] = L, Limit) when length(L) == Limit ->
   F;
 speak([MostRecentlySpokenNumber|Rest] = L, Limit) ->
   case indexof(MostRecentlySpokenNumber, Rest) of
-    false ->
-      %% Number not spoken before
-      speak([0|L], Limit);
-    I ->
-      %% Number was spoken before, compute number of turns
-      %% apart of the last two times it was spoken.
-      speak([I + 1|L], Limit)
+    false -> speak([0|L], Limit);
+    I -> speak([I + 1|L], Limit)
   end.
 
 indexof(E, L) -> indexof(E, 0, L).
@@ -28,9 +23,49 @@ indexof(E, I, [E|_]) -> I;
 indexof(E, I, [_|Rest]) -> indexof(E, I + 1, Rest).
 
 
-part2(_Input) ->
-  ?debugMsg("Not implemented."),
-  0.
+part2(List, Limit) ->
+  %% Map from turn# to the number spoken during that turn
+  TurnToNumberMap = maps:from_list(
+                      lists:zip(lists:seq(1, length(List)),
+                                List)),
+
+  %% Map from numbers to a list of the turns that number was spoken in
+  NumberToTurnsMap =
+    lists:foldl(fun({Turn, Num}, Acc) ->
+                    maps:update_with(Num,
+                                     fun(Old) -> [Turn|Old] end,
+                                     [Turn], Acc)
+                end, #{},
+                lists:zip(lists:seq(1, length(List)),
+                          List)),
+
+  LastSpoken = lists:last(List),
+  speak2(LastSpoken, TurnToNumberMap, NumberToTurnsMap, Limit).
+
+speak2(LastSpoken, TurnToNumberMap, NumberToTurnsMap, Limit) ->
+
+  Turns = maps:size(TurnToNumberMap),
+
+  case Turns of
+    T when T == Limit ->
+      LastSpoken;
+    _ ->
+      NextSpoken =
+        case maps:get(LastSpoken, NumberToTurnsMap, undefined) of
+          [_] -> 0;
+          [T1,T2|_] ->
+            T1 - T2
+        end,
+
+      Turn = Turns + 1,
+
+      speak2(NextSpoken,
+             maps:put(Turn, NextSpoken, TurnToNumberMap),
+             maps:update_with(NextSpoken,
+                              fun([Old|_]) -> [Turn, Old] end, % only keep last two
+                              [Turn], NumberToTurnsMap),
+             Limit)
+  end.
 
 input() ->
   [6,4,12,1,20,0,16].
@@ -40,7 +75,7 @@ main_test_() ->
   Input = input(),
 
   [ {"Part 1", ?_assertEqual(475, part1(Input))}
-  , {"Part 2", ?_assertEqual(0, part2(Input))}
+  , {timeout, 10000, {"Part 2", ?_assertEqual(11261, part2(Input, 30000000))}}
   ].
 
 indexof_test_() ->
@@ -49,8 +84,10 @@ indexof_test_() ->
   ].
 
 ex1_test_() ->
-  [ ?_assertEqual(436, part1([0, 3, 6]))
-  ].
+  ?_assertEqual(436, part1([0, 3, 6])).
+
+ex2_test_() ->
+  ?_assertEqual(436, part2([0, 3, 6], 2020)).
 
 %%%_* Emacs ====================================================================
 %%% Local Variables:
