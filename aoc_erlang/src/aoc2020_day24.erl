fc30a18fa9f59000d09c9891a80016ca9c080cb0
Jesper Eskilson <jesper.eskilson@klarna.com>
2020-12-24 07:28:11 +0100

    2020 day 24 solutions

--- aoc_erlang/src/aoc2020_day24.erl
+++ aoc_erlang/src/aoc2020_day24.erl
@@ -1,53 +1,152 @@
 %%% Advent of Code solution for 2020 day 24.
-%%% Created: 2020-12-22T21:10:14+00:00
+%%% Created: 2020-12-24T05:02:46+00:00
 
 -module(aoc2020_day24).
--behavior(aoc_puzzle).
-
--export([info/0,
-         solve/1,
-         parse/1]).
-
--include("aoc_puzzle.hrl").
-
-%% ======================================================================
-%% Info
-%% ======================================================================
--spec info() -> aoc_puzzle().
-info() ->
-  #aoc_puzzle{module = ?MODULE,
-              year = 2020,
-              day = 24,
-              name = "AoC 2020 day 24"}.
-
-%% ======================================================================
-%% Solver
-%% ======================================================================
--spec solve(ParsedInput :: term()) -> ok.
-solve(_Input) ->
-  timer:sleep(1000),
-  ok.
-
-%% ======================================================================
-%% Parser
-%% ======================================================================
--spec parse(Input :: binary()) -> ParsedInput :: term().
-parse(Input) ->
-  Input.
-
-%% ======================================================================
-%% Tests
-%% ======================================================================
-
--ifdef(EUNIT).
 -include_lib("eunit/include/eunit.hrl").
 
-main_test_() ->
-  _Input = aoc_puzzle:get_parsed_input(?MODULE),
-  %% ...
-  ?_assert(false).
+%% For an explanation of how to use 3D coords to represent hexagons,
+%% https://www.redblobgames.com/grids/hexagons/
 
--endif.
+%% Puzzle solution
+part1(Input) ->
+  Tiles = flip_tiles(Input),
+  maps:size(Tiles).
+
+flip_tiles(Input) ->
+  lists:foldl(
+    fun(Line, Acc) ->
+        FinalCoord =
+          fold_coords(
+            fun(Dir, {X, Y, Z}) ->
+                %% Follow the conventions from
+                %% https://www.redblobgames.com/grids/hexagons/#neighbors
+                %% Note: sum of all the +1/-1 must be zero.
+                case Dir of
+                  "ne" -> {X + 1, Y,     Z - 1};
+                  "e"  -> {X + 1, Y - 1, Z    };
+                  "se" -> {X,     Y - 1, Z + 1};
+                  "sw" -> {X - 1, Y,     Z + 1};
+                  "w"  -> {X - 1, Y + 1, Z    };
+                  "nw" -> {X,     Y + 1, Z - 1}
+                end
+            end, {0, 0, 0}, Line),
+
+        %% Only store black keys, all other tiles are white
+        case maps:is_key(FinalCoord, Acc) of
+          true -> maps:remove(FinalCoord, Acc);
+          false -> maps:put(FinalCoord, black, Acc)
+        end
+
+    end, #{}, Input).
+
+%% Fold over a hexagonal direction strings as found in the input
+fold_coords(_Fun, State, []) ->
+  State;
+fold_coords(Fun, State, [A, B|Rest]) when ([A, B] =:= "ne") orelse
+                                          ([A, B] =:= "nw") orelse
+                                          ([A, B] =:= "sw") orelse
+                                          ([A, B] =:= "se") ->
+  fold_coords(Fun, Fun([A, B], State), Rest);
+fold_coords(Fun, State, [A|Rest]) when ([A] =:= "e") orelse
+                                       ([A] =:= "w") ->
+  fold_coords(Fun, Fun([A], State), Rest).
+
+%% ======================================================================
+%% Part 2
+part2(Input, N) ->
+  Tiles = flip_tiles(Input),
+  Final = lists:foldl(fun do_one_iter/2, Tiles, lists:seq(1, N)),
+  maps:size(Final).
+
+do_one_iter(_N, Tiles) ->
+  Coords =
+    sets:to_list(
+      maps:fold(
+        fun(Coord, _, Acc) ->
+            sets:union(Acc, sets:from_list([Coord|neighbors(Coord)]))
+        end, sets:new(), Tiles)),
+
+  lists:foldl(
+    fun(Coord, Acc) ->
+        Neighbors = neighbors(Coord),
+        IsBlack = maps:is_key(Coord, Tiles),
+        case {IsBlack, count_black_neighbors(Neighbors, Tiles)} of
+          {true, 0} -> maps:remove(Coord, Acc);
+          {true, NB} when NB > 2 -> maps:remove(Coord, Acc);
+          {false, 2} -> maps:put(Coord, black, Acc);
+          _ -> Acc
+        end
+    end, Tiles, Coords).
+
+count_black_neighbors(Neighbors, Tiles) ->
+  lists:foldl(
+    fun(Coord, N) ->
+        case maps:get(Coord, Tiles, white) of
+          black -> N + 1;
+          _ -> N
+        end
+    end, 0, Neighbors).
+
+neighbors({X, Y, Z}) ->
+  [{X + Dx, Y + Dy, Z + Dz} ||
+    Dx <- [-1, 0, 1],
+    Dy <- [-1, 0, 1],
+    Dz <- [-1, 0, 1],
+    {Dy, Dx, Dz} =/= {0, 0, 0},
+    Dy + Dx + Dz == 0].
+
+%% Input reader (place downloaded input file in
+%% priv/inputs/2020/input24.txt).
+get_input() ->
+  inputs:get_as_lines(2020, 24).
+
+%% Tests
+main_test_() ->
+  Input = get_input(),
+
+  [ {"Part 1", ?_assertEqual(300, part1(Input))}
+  , {"Part 2", ?_assertEqual(3466, part2(Input, 100))}
+  ].
+
+neighbors_test_() ->
+  ?_assertEqual([{-1,0,1},
+                 {-1,1,0},
+                 {0,-1,1},
+                 {0,1,-1},
+                 {1,-1,0},
+                 {1,0,-1}], lists:sort(neighbors({0, 0, 0}))).
+
+fold_coords_test_() ->
+  ?_assertEqual(["e", "se", "ne", "e"],
+                fold_coords(fun(Dir, Acc) ->
+                                Acc ++ [Dir]
+                            end, [], "esenee")).
+
+test_input() ->
+  [ "sesenwnenenewseeswwswswwnenewsewsw"
+  , "neeenesenwnwwswnenewnwwsewnenwseswesw"
+  , "seswneswswsenwwnwse"
+  , "nwnwneseeswswnenewneswwnewseswneseene"
+  , "swweswneswnenwsewnwneneseenw"
+  , "eesenwseswswnenwswnwnwsewwnwsene"
+  , "sewnenenenesenwsewnenwwwse"
+  , "wenwwweseeeweswwwnwwe"
+  , "wsweesenenewnwwnwsenewsenwwsesesenwne"
+  , "neeswseenwwswnwswswnw"
+  , "nenwswwsewswnenenewsenwsenwnesesenew"
+  , "enewnwewneswsewnwswenweswnenwsenwsw"
+  , "sweneswneswneneenwnewenewwneswswnese"
+  , "swwesenesewenwneswnwwneseswwne"
+  , "enesenwswwswneneswsenwnewswseenwsese"
+  , "wnwnesenesenenwwnenwsewesewsesesew"
+  , "nenewswnwewswnenesenwnesewesw"
+  , "eneswnwswnwsenenwnwnwwseeswneewsenese"
+  , "neswnwewnwnwseenwseesewsenwsweewe"
+  , "wseweeenwnesenwwwswnew"
+  ].
+
+ex1_test_() ->
+  ?_assertEqual(23, part2(test_input(), 5)).
 
 %%%_* Emacs ====================================================================
 %%% Local Variables:
