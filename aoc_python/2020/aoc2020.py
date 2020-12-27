742064bf21ee67fbbee4bf4cb1f083908d02daf0
Jesper Eskilson <jesper.eskilson@klarna.com>
2020-12-27 14:08:20 +0100

    more work on 2020 python solutions

--- aoc_python/2020/aoc2020.py
+++ aoc_python/2020/aoc2020.py
@@ -6,27 +6,24 @@ import day03
 
 from tabulate import tabulate
 
-def isalambda(v):
-  LAMBDA = lambda:0
-  return isinstance(v, type(LAMBDA)) and v.__name__ == LAMBDA.__name__
-
 puzzles = [ day01.Day01(),
             day02.Day02(),
             day03.Day03() ]
 
 def ts(t):
     ms = t * 1000
-    if ms < 1:
-        us = t * 1000000
-        return f"{us:g} us"
-    else:
-        return f"{ms:g} ms"
+    return f"{ms:.3g} ms"
 
 if __name__ == "__main__":
     for p in puzzles:
         p.run()
 
-    Table = [[p.year,
+    sum_parse = sum([p.parse_time for p in puzzles])
+    sum_p1 = sum([p.p1_time for p in puzzles])
+    sum_p2 = sum([p.p2_time for p in puzzles])
+    sum_total = sum([p.total_time for p in puzzles])
+
+    table = [[p.year,
               p.day,
               ts(p.parse_time),
               ts(p.p1_time),
@@ -35,8 +32,25 @@ if __name__ == "__main__":
               p.part1,
               p.part2,
               p.status
-              ] for p in puzzles]
+              ] for p in puzzles] + \
+              [["Total",
+                "",
+                ts(sum_parse),
+                ts(sum_p1),
+                ts(sum_p2),
+                ts(sum_total),
+                "",
+                "",
+                ""]]
 
-    print(tabulate(Table,
-                   headers = ["Year", "Day", "Parse time", "Part 1", "Part 2", "Total", "Answer (p1)", "Answer (p2)", "Status"],
+    print(tabulate(table,
+                   headers = ["Year",
+                              "Day",
+                              "Parse time",
+                              "Part 1",
+                              "Part 2",
+                              "Total",
+                              "Answer (p1)",
+                              "Answer (p2)",
+                              "Status"],
                    tablefmt = "fancy_grid"))
--- aoc_python/2020/day03.py
+++ aoc_python/2020/day03.py
@@ -1,19 +1,34 @@
 import re
 
 from aoccommon import AocPuzzle
+from functools import reduce
 
 class Day03(AocPuzzle):
     def id(self):
         return (2020, 3)
 
     def parse(self, io):
-        pass
+        return io.readlines()
 
     def solve1(self, data):
-        pass
+        return self.tobogganing(data, 3, 1)
 
     def solve2(self, data):
-        pass
+        return reduce(lambda x, y: x*y,
+                      [self.tobogganing(data, dx, dy)
+                       for dx, dy in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]])
+
+    def tobogganing(self, data, dx, dy):
+        (x, y) = (0, 0)
+        trees = 0
+        width = len(data[0].strip())
+        while y < len(data):
+            if data[y][x] == "#":
+                trees += 1
+            x = (x + dx) % width
+            y = (y + dy)
+
+        return trees
 
     def expected(self):
         return (230, 9533698720)
