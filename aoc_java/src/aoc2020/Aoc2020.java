169c604fbff1a3789bce32cf91753f86bb3cc204
Jesper Eskilson <jesper.eskilson@klarna.com>
2021-01-02 11:13:09 +0100

    2020 day 10solution in java

--- aoc_java/src/aoc2020/Aoc2020.java
+++ aoc_java/src/aoc2020/Aoc2020.java
@@ -20,6 +20,7 @@ import aoc2020.solutions.Day06;
 import aoc2020.solutions.Day07;
 import aoc2020.solutions.Day08;
 import aoc2020.solutions.Day09;
+import aoc2020.solutions.Day10;
 import aoc2020.solutions.Day11;
 import aoc2020.solutions.Day15;
 
@@ -38,6 +39,7 @@ public class Aoc2020 {
         puzzles.add(new Day07());
         puzzles.add(new Day08());
         puzzles.add(new Day09());
+        puzzles.add(new Day10());
         puzzles.add(new Day11());
         puzzles.add(new Day15());
         // =====================================================
