80ec7fc871afd8e91766a3d34a6b411dbc980933
Jesper Eskilson <jesper.eskilson@klarna.com>
2021-01-05 00:13:30 +0100

    optimize 2020 day 23 java solution

--- aoc_java/src/aoc2020/solutions/Day23.java
+++ aoc_java/src/aoc2020/solutions/Day23.java
@@ -1,13 +1,8 @@
 package aoc2020.solutions;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import aoc2020.AocBaseRunner;
 import aoc2020.AocPuzzleInfo;
@@ -17,7 +12,7 @@ import aoc2020.solutions.Day23.Cups;
 
 public class Day23 implements IAocIntPuzzle<Cups> {
 
-    record Cups(Map<Long, Long> cups, long current, long last) {
+    record Cups(int[] ring, int[] input) {
     }
 
     @Override
@@ -32,89 +27,84 @@ public class Day23 implements IAocIntPuzzle<Cups> {
 
     @Override
     public Cups parse(Optional<File> file) {
-        long[] input = { 5L, 8L, 6L, 4L, 3L, 9L, 1L, 7L, 2L };
-        Map<Long, Long> map = new HashMap<>();
+        /*
+         * Represent the ring of cups as an integer array. Java's collection
+         * types are too expensive; we would need to create 1M objects (for part
+         * 2).
+         */
+        int[] input = new int[] { 5, 8, 6, 4, 3, 9, 1, 7, 2 };
+        int[] ring = new int[input.length + 1];
         for (int i = 0; i < input.length - 1; i++) {
-            map.put(input[i], input[i + 1]);
+            ring[input[i]] = input[i + 1];
         }
-        map.put(input[input.length - 1], input[0]);
-        return new Cups(map, input[0], input[input.length - 1]);
+        ring[input[input.length - 1]] = input[0];
+        return new Cups(ring, input);
     }
 
     @Override
     public Long part1(Cups input) {
-        Map<Long, Long> cups = new HashMap<>();
-        cups.putAll(input.cups);
-        var current = input.current;
-        var stats = input.cups.values().stream()
-                .collect(Collectors.summarizingLong(n -> n));
-        var max = stats.getMax();
-        repeat(cups, current, 100, max);
-        return labels(cups, 1);
-    }
-
-    private static void repeat(final Map<Long, Long> map, long current,
-            final long repeat, final long max) {
-
-        /*
-         * TODO rewrite this using a long array instead to avoid unboxing
-         */
-        for (long i = 0; i < repeat; i++) {
-            var a = map.get(current);
-            var b = map.get(a);
-            var c = map.get(b);
-            var next = map.get(c);
-            map.remove(a);
-            map.remove(b);
-            map.remove(c);
-            map.put(current, next);
-            // var dest = findDest(map, current, max);
-            long dest = current;
-            do {
-                dest = (dest == 1) ? max : dest - 1;
-            } while (!map.containsKey(dest));
-            var destNext = map.get(dest);
-            map.put(dest, a);
-            map.put(a, b);
-            map.put(b, c);
-            map.put(c, destNext);
-            current = next;
-        }
-    }
-
-    private static long labels(Map<Long, Long> map, long start) {
-        StringBuilder b = new StringBuilder();
-        for (long i = 0,
-                n = map.get(start); i < map.size() - 1; i++, n = map.get(n)) {
-            b.append(n);
-        }
-        return Long.parseLong(b.toString());
+        int[] ring = input.ring.clone();
+        repeat(ring, input.input[0], 100, 9);
+        return labels(ring, 1);
     }
 
     @Override
     public Long part2(Cups input) {
-        Map<Long, Long> cups = new HashMap<>();
-        cups.putAll(input.cups);
+        var size = 1_000_000;
+        var repeats = 10_000_000;
 
-        var size = 1_000_000L;
-        var repeats = 10_000_000L;
-        var current = input.current;
-        var max = size;
+        // Make a 1M integer array to store the ring in as a linked list.
+        int[] ring = new int[size + 1];
 
-        // Fill up the rest of the map
-        var fillStart = (long) cups.size() + 1;
-        cups.put(input.last, fillStart);
-        for (long i = fillStart; i < size; i++) {
-            cups.put(i, i + 1);
+        // Fill it up with integers
+        var fillStart = input.input.length + 1;
+        for (int i = 0; i < input.ring.length; i++) {
+            ring[i] = input.ring[i];
         }
-        cups.put(size, current);
+        int last = input.input[input.input.length - 1];
+        ring[last] = fillStart;
+        for (int i = fillStart; i < ring.length - 1; i++) {
+            ring[i] = i + 1;
+        }
+        ring[ring.length - 1] = input.input[0];
 
-        assertEquals(size, cups.size());
-        repeat(cups, current, repeats, max);
+        // Play crab cups for 10M rounds.
+        repeat(ring, input.input[0], repeats, size);
 
-        var a = cups.get(1L);
-        var b = cups.get(a);
-        return a * b;
+        int a = ring[1];
+        int b = ring[a];
+        return (long) a * (long) b;
+    }
+
+    private static long labels(int[] cups, int start) {
+        StringBuilder b = new StringBuilder();
+        for (int i = cups[start]; i != start; i = cups[i]) {
+            b.append(i);
+        }
+        return Long.parseLong(b.toString());
+    }
+
+    private static void repeat(int[] ring, int current, int repeat, int max) {
+        for (long i = 0; i < repeat; i++) {
+            var a = ring[current];
+            var b = ring[a];
+            var c = ring[b];
+            var next = ring[c];
+            ring[a] = -1;
+            ring[b] = -1;
+            ring[c] = -1;
+            ring[current] = next;
+            var dest = current;
+            do {
+                dest = (dest == 1) ? max : dest - 1;
+            } while (ring[dest] == -1);
+            var destNext = ring[dest];
+            ring[dest] = a;
+            ring[a] = b;
+            ring[b] = c;
+            ring[c] = destNext;
+            current = next;
+        }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
