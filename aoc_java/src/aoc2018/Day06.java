583d2e352875284cd08ad01b9d65192b1cf8e9e2
Jesper Eskilson <jesper.eskilson@klarna.com>
2021-02-04 19:12:18 +0100

    2018 java solutions

--- aoc_java/src/aoc2018/Day06.java
+++ aoc_java/src/aoc2018/Day06.java
@@ -1,6 +1,6 @@
 package aoc2018;
 
-import static common2.MapUtils.mapValueComparator;
+import static java.lang.Math.abs;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,6 +20,7 @@ import common2.AocPuzzleInfo;
 import common2.AocResult;
 import common2.IAocIntPuzzle;
 import common2.InputUtils;
+import common2.MapUtils;
 
 public class Day06 implements IAocIntPuzzle<List<Coord>> {
 
@@ -56,24 +57,31 @@ public class Day06 implements IAocIntPuzzle<List<Coord>> {
 
     @Override
     public Integer part1(List<Coord> input) {
-        int maxx = input.stream().mapToInt(c -> c.x).max().getAsInt();
-        int maxy = input.stream().mapToInt(c -> c.y).max().getAsInt();
+        int maxx = 356;
+        int maxy = 353;
         Set<Integer> infiniteAreas = new HashSet<>();
         Map<Integer, Integer> areas = new HashMap<>();
 
         for (int x = 0; x < maxx; x++) {
             for (int y = 0; y < maxy; y++) {
-                int x0 = x;
-                int y0 = y;
-                var byDistance = input.stream().map(c -> {
-                    int d = Math.abs(c.x - x0) + Math.abs(c.y - y0);
-                    return new CoordDist(c.x, c.y, d, c.id);
-                }).sorted().limit(2).collect(Collectors.toList());
+                Coord closest = null; // closest coordinate
+                int d0 = Integer.MAX_VALUE; // distance to closest coord
+                int d1 = Integer.MAX_VALUE; // distance to next-closest coord
 
-                CoordDist closest = byDistance.get(0);
-                CoordDist nextClosest = byDistance.get(1);
+                // Check which coordinate is closest, and also track if
+                // there is a tie
+                for (Coord c : input) {
+                    int d = abs(c.x - x) + abs(c.y - y);
+                    if (d < d0) {
+                        closest = c;
+                        d1 = d0;
+                        d0 = d;
+                    } else if (d < d1) {
+                        d1 = d;
+                    }
+                }
 
-                if (closest.dist != nextClosest.dist) {
+                if (d1 != d0) {
                     if (x == 0 || x == maxx - 1 || y == 0 || y == maxy - 1) {
                         infiniteAreas.add(closest.id);
                     } else {
@@ -86,20 +94,20 @@ public class Day06 implements IAocIntPuzzle<List<Coord>> {
 
         return areas.entrySet().stream()
                 .filter(e -> !infiniteAreas.contains(e.getKey()))
-                .sorted(Collections.reverseOrder(mapValueComparator()))
+                .sorted(Collections.reverseOrder(MapUtils.mapValueComparator()))
                 .findFirst().get().getValue();
     }
 
     @Override
     public Integer part2(List<Coord> input) {
-        int maxx = input.stream().mapToInt(c -> c.x).max().getAsInt();
-        int maxy = input.stream().mapToInt(c -> c.y).max().getAsInt();
+        int maxx = 356;
+        int maxy = 353;
         int size = 0;
         for (int x = 0; x < maxx; x++) {
             for (int y = 0; y < maxy; y++) {
                 int d = 0;
                 for (Coord c : input) {
-                    d += (Math.abs(c.x - x) + Math.abs(c.y - y));
+                    d += (abs(c.x - x) + abs(c.y - y));
                 }
                 if (d < 10000) {
                     size++;
